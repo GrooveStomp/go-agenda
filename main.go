10e3401568d133a8f3fff960b70bea94d4fcdf12
Aaron Oman <aaron.oman@lendesk.com>
2018-05-31 14:21:33 -0700

    Help dialog

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
+	"strings"
 )
 
 func main() {
@@ -19,20 +20,44 @@ func main() {
 	list.AddItem("Example 4", "Description 4", 0, nil)
 	list.AddItem("Example 5", "Description 5", 0, nil)
 
+	help := tview.NewTextView()
+	help.SetBorder(true)
+	help.SetTitle("help")
+	help.SetText(strings.TrimSpace(`
+?        Show this help text.
++        Add a new item.
+x        Mark an item as complete.
+<tab>    Expand an item.
+<ctrl+c> Quit
+<esc>    Quit any popups, dialogs or modals.
+`))
+
 	flex := tview.NewFlex()
 	flex.SetFullScreen(true)
 	flex.SetDirection(tview.FlexColumn)
 	flex.AddItem(list, 0, 1, true)
 
+	pages := tview.NewPages()
+	pages.AddPage("main", flex, true, true)
+	pages.AddPage("help", help, true, false)
+
 	boxShown := false
+	helpShown := false
+	currentPage := "main"
+	lastPage := "main"
 
 	app := tview.NewApplication()
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	app.SetInputCapture(func(event *tcell.EventKey) (result *tcell.EventKey) {
+		result = event
 		if event.Key() != tcell.KeyRune {
-			return event
+			return
 		}
 
 		if event.Rune() == 't' {
+			if currentPage != "main" {
+				return
+			}
+			
 			if boxShown {
 				flex.RemoveItem(box)
 				boxShown = false
@@ -40,13 +65,28 @@ func main() {
 				flex.AddItem(box, 0, 1, true)
 				boxShown = true
 			}
-			app.Draw()
-			return nil
+			result = nil
+		} else if event.Rune() == '?' {
+			if helpShown {
+				tmp := currentPage
+				currentPage = lastPage
+				lastPage = tmp
+				pages.SwitchToPage(currentPage)
+				helpShown = false
+			} else {
+				lastPage = currentPage
+				currentPage = "help"
+				pages.SwitchToPage(currentPage)
+				helpShown = true
+			}
 		}
-		return event
+		
+		app.Draw()
+
+		return
 	})
 
-	if err := app.SetRoot(flex, true).Run(); err != nil {
+	if err := app.SetRoot(pages, true).Run(); err != nil {
 		panic(err)
 	}
 }
