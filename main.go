aadbb10df5fe3238faf4eba9c7e63ac1bcd6a648
GrooveStomp <aaron.oman@gmail.com>
2018-06-04 00:04:46 -0700

    WIP

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
+/*
+
+Org-Mode is a hierarchical outline.
+It consists of headlines and collapsing portions of the document.
+
+When I use org-mode, there are a few specific features I make use of:
+- title
+- tags
+- description
+
+Part of what makes org-mode good to use is that you can quickly and arbitrarily
+define nested sections.  Adding a new nested section should be quick and
+painless, and moving sections around (indenting, outdenting, removing as a
+headline (collapsing to parent's level) should also feel natural.
+
+*/
+
 import (
+	"fmt"
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 	"strings"
 )
+
+type agendaItem struct {
+	Title  string
+	Bodies []string // We can have N bodies separated by N-1 Links to other agenda items.
+	Links  []agendaItem
+	Tags   []string
+}
 
 type inputHandler func(*tcell.EventKey) *tcell.EventKey
 
@@ -71,6 +96,7 @@ x        Mark an item as complete.
 	helpShown := false
 	currentPage := "main"
 	lastPage := "main"
+	addItemCount := 0
 
 	help.SetInputCapture(createEscHandler(func() {
 		currentPage = lastPage
@@ -99,13 +125,20 @@ x        Mark an item as complete.
 				result = nil
 				debugOut.SetText("Showing Box")
 			}
-		} else if event.Rune() == '?' {
+		} else if event.Rune() == '?' && !helpShown {
 			lastPage = currentPage
 			currentPage = "help"
 			pages.SwitchToPage(currentPage)
 			helpShown = true
 			result = nil
 			debugOut.SetText("Showing Help")
+		} else if event.Rune() == '+' {
+			addItemCount += 1
+			name := addItemPage(pages, addItemCount)
+			lastPage = currentPage
+			currentPage = name
+			result = nil
+			debugOut.SetText(fmt.Sprintf("Showing %s", name))
 		}
 
 		app.Draw()
@@ -113,22 +146,83 @@ x        Mark an item as complete.
 		return
 	})
 
-	// box.SetInputCapture(func(eventKey *tcell.EventKey) *tcell.EventKey {
-	// 	if eventKey.Key() == tcell.KeyRune && eventKey.Rune() == 't' {
-	// 		flex.RemoveItem(box)
-	// 		boxShown = false
-	// 		debugOut.SetText("Exiting Box")
-	// 		return nil
+	// NOTE(AARON): This input capture doesn't seem to do anything.
+	// pages.SetInputCapture(func(eventKey *tcell.EventKey) (resultKey *tcell.EventKey) {
+	// 	resultKey = eventKey
+	// 	if eventKey.Key() == tcell.KeyRune && eventKey.Rune() == '?' && !helpShown {
+	// 		lastPage = currentPage
+	// 		currentPage = "help"
+	// 		pages.SwitchToPage(currentPage)
+	// 		helpShown = true
+	// 		resultKey = nil
+	// 		debugOut.SetText("Showing Help")
 	// 	}
-	// 	return eventKey
+	// 	return
 	// })
+
+	// NOTE(AARON): This input capture doesn't seem to do anything.
+	// pages.SetInputCapture(func(eventKey *tcell.EventKey) (resultKey *tcell.EventKey) {
+	// 	resultKey = eventKey
+	// 	if eventKey.Key() == tcell.KeyRune && eventKey.Rune() == 't' && !boxShown {
+	// 		flex.AddItem(box, 0, 1, true)
+	// 		app.SetFocus(box)
+	// 		boxShown = true
+	// 		debugOut.SetText("Showing Box")
+	// 		resultKey = nil
+	// 	}
+	// 	return
+	// })
+
 	box.SetInputCapture(createEscHandler(func() {
+		debugOut.SetText("Exiting Box")
 		flex.RemoveItem(box)
 		boxShown = false
-		debugOut.SetText("Exiting Box")
+		debugOut.SetText("Box Exited")
 	}))
 
+	app.SetFocus(pages)
 	if err := app.SetRoot(mainGrid, true).Run(); err != nil {
 		panic(err)
 	}
+}
+
+func addItemPage(pages *tview.Pages, dialogNum int) string {
+	/*
+		This should have its own input handler to:
+		- tab between elements.
+		- record data in a common datastructure.
+	*/
+	name := fmt.Sprintf("addAgendaItem%v", dialogNum)
+
+	grid := tview.NewGrid()
+	grid.SetRows(3, -1, 3)
+	grid.SetColumns(-1)
+
+	title := tview.NewInputField()
+	title.SetBorder(true)
+	title.SetTitle("List String")
+
+	body := tview.NewInputField()
+	body.SetBorder(true)
+	body.SetTitle("Full Description")
+
+	tags := tview.NewInputField()
+	tags.SetBorder(true)
+	tags.SetTitle("Tags")
+	tags.SetPlaceholder("tag1 tag-2 spaces_separate_tags")
+
+	grid.AddItem(title, 0, 0, 1, 1, 1, 1, true)
+	grid.AddItem(body, 1, 0, 1, 1, 1, 1, false)
+	grid.AddItem(tags, 2, 0, 1, 1, 1, 1, false)
+
+	// NOTE(AARONO): This input handler doesn't work when attached at this level.
+	grid.SetInputCapture(createEscHandler(func() {
+		debugOut.SetText(fmt.Sprintf("Exiting %v", name))
+		pages.RemovePage(name)
+	}))
+
+	pages.AddPage(name, grid, true, false)
+	pages.SwitchToPage(name)
+
+	return name
 }
